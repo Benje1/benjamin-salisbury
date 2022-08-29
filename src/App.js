70390c082bc69803238d235f5cf81963fc3a247b
benjaminsal92@gmail.com <benjaminsal92@gmail.com>
2022-08-29 12:20:22 +0100

    added a tag

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
   return (<>
   <h1>Under Construction</h1>
   <p>To be added later</p>
+  <a href="https://github.com/Benje1?tab=repositories">My github repositories</a>
   </>);
 }
 
