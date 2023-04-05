a1a2e483df1c65a30e6dd9383d0950c974b7e4c5
Benje1 <benjaminsal92@gmail.com>
2023-04-05 15:50:56 +0100

    Small addition

--- src/App.js
+++ src/App.js
@@ -4,9 +4,16 @@ import './App.css';
 
 function App() {
   return (<>
-  <h1>Under Construction</h1>
-  <p>Benjamin Salisbury</p>
-  <a href="https://github.com/Benje1?tab=repositories">My github repositories</a>
+  <p id='name'>Benjamin Salisbury</p>
+  <ul>
+    <a href="https://github.com/Benje1">My github repositories</a>
+    <p>My other projecs are displaid </p>
+    <p>  </p>
+    <a href='https://benje1.github.io/BLACKJACK_Group_Project/'>Blackjack group project</a>
+    <p>You can play blackjack agaist the dealer, play some fun minigames to earn credit to use in the main game </p>
+    <a href='https://seven-minute-test.vercel.app/'>Seven Minute Test</a>
+    <p>This is a simple app that I have developed in conjunction with my brother for his workplace, testing the speed of a bartender to make seven drinks</p>
+  </ul>
   </>);
 }
 
